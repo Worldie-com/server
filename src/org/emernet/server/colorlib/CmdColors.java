63ac385173da7ebf473c6bb615b9599604fa52e0
Christoph <christoph@hour26.com>
2019-11-15 12:09:29 +0100

    Offloaded Colors into colorlib.CmdColors

---
+++ src/org/emernet/server/colorlib/CmdColors.java
@@ -0,0 +1,15 @@
+package org.emernet.server.colorlib;
+
+public class CmdColors {
+
+    public static final String CMD_RESET = "\u001B[0m";
+    public static final String CMD_BLACK = "\u001B[30m";
+    public static final String CMD_RED = "\u001B[31m";
+    public static final String CMD_GREEN = "\u001B[32m";
+    public static final String CMD_YELLOW = "\u001B[33m";
+    public static final String CMD_BLUE = "\u001B[34m";
+    public static final String CMD_PURPLE = "\u001B[35m";
+    public static final String CMD_CYAN = "\u001B[36m";
+    public static final String CMD_WHITE = "\u001B[37m";
+    
+}
