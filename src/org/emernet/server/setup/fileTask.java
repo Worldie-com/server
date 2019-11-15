1118099d45be698941b405c4d8556888dd1d5c3d
Christoph <christoph@hour26.com>
2019-11-15 14:14:27 +0100

    Extended getConfig
    getConfig is now able to get the config file from github.
    It will also move it, delete the default config and restart apache.

--- src/org/emernet/server/setup/fileTask.java
+++ src/org/emernet/server/setup/fileTask.java
@@ -15,7 +15,19 @@ public class fileTask {
     }
 
     public static void getConfig(){
-
+        try {
+            // Get Config File from Github
+            //TODO: Change to master branch as soon as branch will be merged
+            Process getConfig = rt.exec("wget https://github.com/emernet-eins/server/blob/implement-setup/emernet.conf");
+            //Move Config to the right place
+            Process moveConfig = rt.exec("sudo mv emernet.conf /etc/apache2/sites-enabled");
+            //remove 000-default.conf
+            Process removeDefault = rt.exec("sudo rm -rf /etc/apache2/sites-enabled/000-default.conf");
+            //Restart apache2
+            Process restartApache = rt.exec("sudo service apache2 restart");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
