01f95403c4410b534f54a70dc1b1d391d4f289cf
Christoph <christoph@hour26.com>
2019-11-15 08:30:07 +0100

    Initial Commit

---
+++ src/org/emernet/server/control/Main.java
@@ -0,0 +1,7 @@
+package org.emernet.server.control;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Welcome to EMERNET-E.I.N.S server!");
+    }
+}
---
+++ src/org/emernet/server/control/init.java
@@ -0,0 +1,14 @@
+package org.emernet.server.control;
+
+import java.io.File;
+
+public class init {
+
+    public static void checkOS(){
+
+    }
+
+    public static void checkSetup(){
+        File isSetup = new File("");
+    }
+}
