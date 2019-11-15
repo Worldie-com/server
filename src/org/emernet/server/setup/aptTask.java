2e8c89e7fad466b8c7ee18d97da7d38a68f35e54
Christoph <christoph@hour26.com>
2019-11-15 12:16:59 +0100

    Added aptTask Class and added empty methods
    Handles installation of apt packages

---
+++ src/org/emernet/server/setup/aptTask.java
@@ -0,0 +1,18 @@
+package org.emernet.server.setup;
+
+import org.emernet.server.colorlib.CmdColors;
+
+public class aptTask {
+    public static void aptUpdate(){
+
+    }
+
+    public static void instApache(){
+
+    }
+
+    public static void instUnzip(){
+        
+    }
+
+}
