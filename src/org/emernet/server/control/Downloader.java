1227923c992b980585747bbdbee42b52384c6f2c
Christoph <christoph@hour26.com>
2019-11-15 14:29:01 +0100

    Extended Downloader, altered checkUpdate
    - Downloader should work now. Checks for latest version, the downloads it.
    - checkUpdate altered: Version Text File link updated

--- src/org/emernet/server/control/Downloader.java
+++ src/org/emernet/server/control/Downloader.java
@@ -1,4 +1,46 @@
 package org.emernet.server.control;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Downloader {
+    public static Runtime rt = Runtime.getRuntime();
+
+    public static void downloadSystem(){
+        //get Latest Version Tag from GitHub
+        String latestVers = "";
+
+        try {
+
+            URL url = new URL("https://github.com/emernet-eins/system/blob/master/version.md");
+
+            // read text returned by server
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                latestVers = line;
+            }
+            in.close();
+
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("I/O Error: " + e.getMessage());
+        }
+
+        //What a link looks like: https://github.com/emernet-eins/system/archive/testing-1.zip
+        //Building link:
+        String versDL = "https://github.com/emernet-eins/system/archive/"+ latestVers +".zip";
+
+        //Download latest Version from releases
+        try {
+            Process dlVers = rt.exec("wget " + versDL);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
