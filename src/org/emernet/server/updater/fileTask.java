adce71c1f27d68f9e9dd04b5024c9154d5e0d067
Christoph <christoph@hour26.com>
2019-11-26 08:17:11 +0100

    Implemented Systeminfo into Updater

--- src/org/emernet/server/updater/fileTask.java
+++ src/org/emernet/server/updater/fileTask.java
@@ -2,9 +2,7 @@ package org.emernet.server.updater;
 
 import org.emernet.server.control.Downloader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class fileTask {
 
@@ -101,6 +99,24 @@ public class fileTask {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void createSysInfo(){
+        File sysInfo = new File("/var/www/emernet/systeminfo.md");
+
+        // Get System Information
+        String SystemInfo = System.getProperty("os.name") + " (" + System.getProperty("os.arch") + ")";
+
+        try {
+            sysInfo.createNewFile();
+
+            BufferedWriter writeInfo = new BufferedWriter(new FileWriter(sysInfo));
+            writeInfo.write(SystemInfo);
+            writeInfo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
