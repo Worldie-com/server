cc418841414fe1911d8705cea2bc22146ae7ee68
Christoph <christoph@hour26.com>
2019-11-27 15:57:01 +0100

    Added Connection Class

---
+++ src/org/emernet/server/control/Connection.java
@@ -0,0 +1,14 @@
+package org.emernet.server.control;
+
+import org.emernet.server.colorlib.CmdColors;
+
+public class Connection {
+    public static void checkConnection(){
+        if(init.isConnected()){
+            System.out.println("Connection established.");
+        }else{
+            System.out.println(CmdColors.CMD_RED + "Entering Lockdown mode..." + CmdColors.CMD_RESET);
+
+        }
+    }
+}
