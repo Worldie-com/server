05549099dab3d515c836f2e699657abf8488506e
Christoph <christoph@hour26.com>
2019-12-10 07:57:29 +0100

    Implemented clilib to Lockdown

--- src/org/emernet/server/control/Lockdown.java
+++ src/org/emernet/server/control/Lockdown.java
@@ -1,19 +1,18 @@
 package org.emernet.server.control;
 
-import org.emernet.server.colorlib.CmdColors;
-
+import dev.miit0o.clilib.lib.TextColors;
 import java.util.concurrent.TimeUnit;
 
 public class Lockdown {
     public static void enterLockdown(){
         System.out.println("\n\n\n\n\n\n");
-        System.out.println(CmdColors.CMD_RED + "Lockdown mode was activated." + CmdColors.CMD_RESET);
-        System.out.println(CmdColors.CMD_RED + "This could be an emergency!" + CmdColors.CMD_RESET);
-        System.out.println(CmdColors.CMD_RED + "Please turn on your radio/tv (if available) to be informed about what's happening!" + CmdColors.CMD_RESET);
+        System.out.println(TextColors.red + "Lockdown mode was activated." + TextColors.reset);
+        System.out.println(TextColors.red + "This could be an emergency!" + TextColors.reset);
+        System.out.println(TextColors.red + "Please turn on your radio/tv (if available) to be informed about what's happening!" + TextColors.reset);
         System.out.println("\n\n");
-        System.out.println(CmdColors.CMD_RED + "Lockdown will check for an Internet connection every 10 Minutes." + CmdColors.CMD_RESET);
+        System.out.println(TextColors.red + "Lockdown will check for an Internet connection every 10 Minutes." + TextColors.reset);
         System.out.println("\n");
-        System.out.println(CmdColors.CMD_RED + "Going to sleep for 10 minutes." + CmdColors.CMD_RESET);
+        System.out.println(TextColors.red + "Going to sleep for 10 minutes." + TextColors.reset);
         try {
             Thread.sleep(TimeUnit.MINUTES.toMillis(10));
         } catch (InterruptedException e) {
@@ -21,12 +20,12 @@ public class Lockdown {
         }
 
         if (init.isConnected()){
-            System.out.println(CmdColors.CMD_GREEN + "Connection established!" + CmdColors.CMD_RESET);
+            System.out.println(TextColors.green + "Connection established!" + TextColors.reset);
             System.out.println("Restarting EMERNET Runtime...");
             Main.main(null);
         }else{
-            System.out.println(CmdColors.CMD_RED + "No connection!" + CmdColors.CMD_RESET);
-            System.out.println(CmdColors.CMD_RED + "Staying in Lockdown..." + CmdColors.CMD_RESET);
+            System.out.println(TextColors.red + "No connection!" + TextColors.reset);
+            System.out.println(TextColors.red + "Staying in Lockdown..." + TextColors.reset);
             enterLockdown();
         }
     }
